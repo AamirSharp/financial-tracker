d678d1d6bafb832432a7125e4dcd86e00cb36559
Aamir <aamirsharp16@gmail.com>
2025-07-25 19:04:47 +0530

    Initial commit of financial tracker app

---
+++ src/anaskfinancialtracker/Fin_table.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anaskfinancialtracker;
+
+/**
+ *
+ * @author aamir
+ */
+class Fin_table {
+
+    static int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
